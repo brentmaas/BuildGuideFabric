8442221bb2fbf734fb3f5b5f99cee14784a28d3a
Brent Maas <brent.maas@hotmail.com>
2021-12-15 18:22:11 +0100

    Reimplementation of Forge 1.18.1-0.3.1

--- src/main/java/brentmaas/buildguide/InputHandler.java
+++ src/main/java/brentmaas/buildguide/InputHandler.java
@@ -5,21 +5,31 @@ import org.lwjgl.glfw.GLFW;
 import brentmaas.buildguide.screen.BuildGuideScreen;
 import brentmaas.buildguide.screen.ShapelistScreen;
 import brentmaas.buildguide.screen.VisualisationScreen;
+import brentmaas.buildguide.shapes.Shape;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.math.Vec3d;
 
 public class InputHandler {
+	private static final String keyCategory = "key.buildguide.category";
+	
 	private static KeyBinding openBuildGuide;
 	private static KeyBinding openShapelist;
 	private static KeyBinding openVisualisation;
+	private static KeyBinding toggleEnable;
+	private static KeyBinding setBasepos;
+	private static KeyBinding setGlobalBasepos;
 	
 	public static void register() {
-		openBuildGuide = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.buildguide.openbuildguide", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_B, "key.buildguide.category"));
-		openShapelist = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.buildguide.openshapelist", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.buildguide.category"));
-		openVisualisation = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.buildguide.openvisualisation", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.buildguide.category"));
+		openBuildGuide = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.buildguide.openbuildguide", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_B, keyCategory));
+		openShapelist = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.buildguide.openshapelist", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, keyCategory));
+		openVisualisation = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.buildguide.openvisualisation", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, keyCategory));
+		toggleEnable = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.buildguide.toggleenable", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, keyCategory));
+		setBasepos = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.buildguide.setbasepos", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, keyCategory));
+		setGlobalBasepos = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.buildguide.setglobalbasepos", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, keyCategory));
 		
 		ClientTickEvents.END_CLIENT_TICK.register(InputHandler::onKeyInput);
 	}
@@ -35,6 +45,25 @@ public class InputHandler {
 		
 		if(openVisualisation.wasPressed()) {
 			MinecraftClient.getInstance().setScreen(new VisualisationScreen());
+		}
+		
+		if(toggleEnable.wasPressed()) {
+			StateManager.getState().initCheck();
+			StateManager.getState().propertyEnable.setValue(!StateManager.getState().propertyEnable.value);
+		}
+		
+		if(setBasepos.wasPressed() && StateManager.getState().isShapeAvailable()) {
+			StateManager.getState().resetBasepos();
+		}
+		
+		if(setGlobalBasepos.wasPressed() && StateManager.getState().propertyAdvancedMode.value && StateManager.getState().isShapeAvailable()) {
+			Vec3d pos = MinecraftClient.getInstance().player.getPos();
+			int deltaX = (int) (Math.floor(pos.x) - StateManager.getState().getCurrentShape().basePos.x);
+			int deltaY = (int) (Math.floor(pos.y) - StateManager.getState().getCurrentShape().basePos.y);
+			int deltaZ = (int) (Math.floor(pos.z) - StateManager.getState().getCurrentShape().basePos.z);
+			for(Shape s: StateManager.getState().advancedModeShapes) {
+				s.shiftBasepos(deltaX, deltaY, deltaZ);
+			}
 		}
 	}
 }
