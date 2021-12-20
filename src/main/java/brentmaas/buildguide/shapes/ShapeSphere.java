f9339dd9b27995619fa9942fd73b0d627a604f82
Brent Maas <brent.maas@hotmail.com>
2021-12-20 16:38:13 +0100

    Reimplementation of Forge 1.18.1-0.3.2

--- src/main/java/brentmaas/buildguide/shapes/ShapeSphere.java
+++ src/main/java/brentmaas/buildguide/shapes/ShapeSphere.java
@@ -18,8 +18,8 @@ public class ShapeSphere extends Shape{
 	
 	private String[] domeNames = {"-", "+X", "+Y", "+Z", "-X", "-Y", "-Z"};
 	
-	private PropertyPositiveInt propertyRadius = new PropertyPositiveInt(0, 3, new TranslatableText("property.buildguide.radius"), () -> this.update());
-	private PropertyEnum<dome> propertyDome = new PropertyEnum<dome>(1, dome.NO, new TranslatableText("property.buildguide.dome"), () -> this.update(), domeNames);
+	private PropertyPositiveInt propertyRadius = new PropertyPositiveInt(0, 3, new TranslatableText("property.buildguide.radius"), () -> update());
+	private PropertyEnum<dome> propertyDome = new PropertyEnum<dome>(1, dome.NO, new TranslatableText("property.buildguide.dome"), () -> update(), domeNames);
 	
 	public ShapeSphere() {
 		super();
